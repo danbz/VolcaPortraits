a06cdd7f488c40b387a904e79257db804f0299e6
Alexander Birke <parahunter@gmail.com>
2018-04-10 22:15:02 +0100

    VR Input

---
+++ Assets/Scripts/TrackedController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace OutOfBounds.SteamVR
+{
+	public class TrackedController : MonoBehaviour
+	{
+		private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+		private SteamVR_TrackedObject trackedObj;
+
+		public static TrackedController left { get; private set; }
+		public static TrackedController right { get; private set; }
+		
+		// Use this for initialization
+		void Start()
+		{
+			trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+			print(name);
+
+			if (name.Contains("left"))
+			{
+				left = this;
+			}
+			if (name.Contains("right"))
+			{
+				right = this;
+			}
+		}
+
+		#region touch pad
+		public bool GetTouchDown()
+		{
+			return controller.GetTouchDown(EVRButtonId.k_EButton_SteamVR_Touchpad);
+        }
+
+		public bool GetTouch()
+		{
+			return controller.GetTouch(EVRButtonId.k_EButton_SteamVR_Touchpad);
+		}
+
+		public bool GetTouchUp()
+		{
+			return controller.GetTouchUp(EVRButtonId.k_EButton_SteamVR_Touchpad);
+		}
+
+		public Vector2 GetTouchPosition()
+		{
+			return controller.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
+		}
+
+		public bool GetTouchPressDown()
+		{
+			return controller.GetPressDown(EVRButtonId.k_EButton_SteamVR_Touchpad);
+		}
+
+		public bool GetTouchPress()
+		{
+			return controller.GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad);
+		}
+
+		public bool GetTouchPressUp()
+		{
+			return controller.GetPressUp(EVRButtonId.k_EButton_SteamVR_Touchpad);
+		}
+
+		#endregion
+		#region trigger
+		public float GetTrigger()
+		{
+			return controller.GetAxis(EVRButtonId.k_EButton_SteamVR_Trigger).y;
+		}
+		
+		public bool GetTriggerDown()
+		{
+			return controller.GetTouchDown(EVRButtonId.k_EButton_SteamVR_Trigger);
+		}
+		
+		public bool GetTriggerUp()
+		{
+			return controller.GetTouchUp(EVRButtonId.k_EButton_SteamVR_Trigger);
+		}
+
+		#endregion
+		#region grip button
+		public bool GetGrip()
+		{
+			return controller.GetPress(EVRButtonId.k_EButton_Grip);
+		}
+
+		public bool GetGripDown()
+		{
+			return controller.GetPressDown(EVRButtonId.k_EButton_Grip);
+		}
+
+		public bool GetGripUp()
+		{
+			return controller.GetPressUp(EVRButtonId.k_EButton_Grip);
+		}
+		#endregion
+		#region application menu button
+		public bool GetApplicationButton()
+		{
+			return controller.GetPress(EVRButtonId.k_EButton_ApplicationMenu);
+		}
+
+		public bool GetApplicationButtonDown()
+		{
+			return controller.GetPressDown(EVRButtonId.k_EButton_ApplicationMenu);
+		}
+
+		public bool GetApplicationButtonUp()
+		{
+			return controller.GetPressUp(EVRButtonId.k_EButton_ApplicationMenu);
+		}
+		
+		#endregion
+	}
+}
---
+++ Assets/Scripts/VRInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OutOfBounds.SteamVR;
+
+public class VRInput : MonoBehaviour
+{
+    
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(TrackedController.left.GetTouchPressDown())
+        {
+            Vector2 touchPos = TrackedController.left.GetTouchPosition();
+
+            if (touchPos.x > 0)
+                GoForward();
+            else
+                GoBack();
+        }
+
+        if (TrackedController.right.GetTouchPressDown())
+        {
+            Vector2 touchPos = TrackedController.right.GetTouchPosition();
+
+            if (touchPos.x > 0)
+                GoForward();
+            else
+                GoBack();
+        }
+    }
+
+    void GoBack()
+    {
+        print("go back");
+    }
+
+    void GoForward()
+    {
+        print("go forward");
+    }
+}
